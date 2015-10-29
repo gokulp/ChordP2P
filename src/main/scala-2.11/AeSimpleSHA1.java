168092a549a772a587a7af8e344ddfb5ceb4090b
Gokul Palwe <gokulpalwe@gmail.com>
2015-10-29 19:10:38 -0400

    hashing for nodeIDs

--- src/main/scala-2.11/AeSimpleSHA1.java
+++ src/main/scala-2.11/AeSimpleSHA1.java
@@ -7,29 +7,21 @@ import java.security.NoSuchAlgorithmException;
 
 public class AeSimpleSHA1 {
 
-    private static String convertToHex(byte[] data) {
-        StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < data.length; i++) {
-            int halfbyte = (data[i] >>> 4) & 0x0F;
-            int two_halfs = 0;
-            do {
-                if ((0 <= halfbyte) && (halfbyte <= 9))
-                    buf.append((char) ('0' + halfbyte));
-                else
-                    buf.append((char) ('a' + (halfbyte - 10)));
-                halfbyte = data[i] & 0x0F;
-            } while(two_halfs++ < 1);
+    private static long convertToInt(byte[] data) {
+        long value = 0;
+        for (int i = 0; i < 4 && i < data.length; i++) {
+            value += ((long) data[i] & 0xffL) << (8 * i);
         }
-        return buf.toString();
+        return value;
     }
 
-    public static String SHA1(String text)
+    public static long SHA1(String text)
             throws NoSuchAlgorithmException, UnsupportedEncodingException  {
         MessageDigest md;
         md = MessageDigest.getInstance("SHA-1");
         byte[] sha1hash = new byte[40];
         md.update(text.getBytes("iso-8859-1"), 0, text.length());
         sha1hash = md.digest();
-        return convertToHex(sha1hash);
+        return convertToInt(sha1hash);
     }
 }
